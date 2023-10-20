bda82e5dc57e5b12ba698f99dc483b512ac72368
John Yakovl <tchuwipeach@yandex.com>
2023-10-20 19:58:51 +0700

    commit

--- base/foundation/apps.py
+++ base/foundation/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AnywayConfig(AppConfig):
+class FoundationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'anyway'
+    name = 'foundation'
