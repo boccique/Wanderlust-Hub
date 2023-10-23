fffde6fab4d64a618a57c1fb5703f22ee51848d6
John Yakovl <tchuwipeach@yandex.com>
2023-10-23 23:15:29 +0700

    commit

--- base/foundation/models.py
+++ base/foundation/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class RegisteredUser(models.Model):
+    username = models.CharField(max_length=300)
+    email = models.EmailField(max_length=300)
+    passwd = models.CharField(max_length=50, blank=True, null=True)
+    fname = models.CharField(max_length=100, blank=True, null=True)
+    lname = models.CharField(max_length=200, blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
+    bio = models.TextField(max_length=10000, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
